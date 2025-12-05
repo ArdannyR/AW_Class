0dc436903f452954c19f6a6ed21b79ee4023afc4
ArdannyR <ardannyromero@gmail.com>
2025-12-05 12:16:48 -0500

    avance de back

--- backend-vet/src/server.js
+++ backend-vet/src/server.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors';
+import routerVeterinarios from './routers/veterinario_routes.js'
 
 // Inicializaciones
 const app = express()
@@ -16,8 +17,14 @@ app.use(cors())
 // Variables globales
 app.set('port',process.env.PORT || 3000)
 
-// Rutas 
-app.get('/',(req,res)=> res.send("Server on"))
+// Ruta principal
+app.get('/',(req,res)=>res.send("Server on"))
+
+// Rutas para veterinarios
+app.use('/api',routerVeterinarios)
+
+// Manejo de una ruta que no sea encontrada
+app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
 
 // Exportar la instancia de express por medio de app
 export default  app
