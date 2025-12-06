b4a82c130e424d653a3c95f2a9de7c862c614d5f
ArdannyR <ardannyromero@gmail.com>
2025-12-05 19:27:39 -0500

    avance de frontend

---
+++ frontend-vet/src/pages/Panel.jsx
@@ -0,0 +1,11 @@
+const Panel = () => {
+    return (
+        <div>
+            <h1 className='font-black text-4xl text-gray-500'>Panel de Control</h1>
+            <hr className='my-4 border-t-2 border-gray-300' />
+            <p className='mb-8'>Bienvenido al sistema de administración veterinaria.</p>
+        </div>
+    )
+}
+
+export default Panel
