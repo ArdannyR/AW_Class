69526d8c8900c0f5362b9b4d5f1037776035d0ff
ArdannyR <ardannyromero@gmail.com>
2025-12-06 12:16:25 -0500

    sprint 2 de back avance

--- backend-vet/src/server.js
+++ backend-vet/src/server.js
@@ -26,11 +26,11 @@ app.get('/',(req,res)=>res.send("Server on"))
 // Rutas para veterinarios
 app.use('/api',routerVeterinarios)
 
-// Manejo de una ruta que no sea encontrada
-app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
-
 // Rutas para pacientes
 app.use('/api',routerPacientes)
+
+// Manejo de una ruta que no sea encontrada
+app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
 
 // Exportar la instancia de express por medio de app
 export default  app
